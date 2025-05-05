446c691e17a7a8514b3aeb542c96d72c127ac2e0
Jackson Grove <jacksongrove@icloud.com>
2025-05-05 11:36:25 -0700

    Bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ dev_requires = test_requires + [
 
 setup(
     name="imagination-engine",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=core_requires,
